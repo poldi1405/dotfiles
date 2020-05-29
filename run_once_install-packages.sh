84b5e1b34f63dca05353938694355e66f25adcf6
Moritz Poldrack <ich@moritz-poldrack.de>
2020-05-29 17:05:47 +0200

    updated install script to reflect changes in vimrc

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -61,12 +61,7 @@ case $input in
 	[nN])
 		;;
 	*)
-		yay -S cmake --noconfirm
-		rm -rf ~/.vim/bundle/Vundle.vim
-		git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-		sudo pacman -S base-devel cmake python vim --noconfirm
-		vim +PluginInstall +GoInstallBinaries +qall
-		cd ~/.vim/bundle/YouCompleteMe
-		python3 install.py --clang-completer --cs-completer --go-completer
+		curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+		vim +PlugInstall
 		;;
 esac
